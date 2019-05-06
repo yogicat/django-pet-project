49f0684ca763ee9c7f1fb347de7d35efdd0ce5f0
yogicat <dahe.oh@gmail.com>
2019-05-06 10:54:29 +0900

    setup fileupload

--- pets/forms.py
+++ pets/forms.py
@@ -7,17 +7,19 @@ class PetCreateForm(forms.ModelForm):
 
     class Meta:
         model = Pet
-        fields = ('name', 'birthday', 'animal', 'registration_number',
+        fields = ('name', 'birthday', 'photo', 'animal', 'registration_number',
                   'diseases_info', 'allergies_info', 'preferences', 'is_main')
+        widgets = {
+            'birthday': forms.SelectDateWidget(years=range(1970, 2020)),
+        }
 
 
 class PetChangeForm(forms.ModelForm):
 
     class Meta:
         model = Pet
-        fields = ('name', 'birthday', 'animal', 'registration_number',
+        fields = ('name', 'birthday', 'photo', 'animal', 'registration_number',
                   'diseases_info', 'allergies_info', 'preferences', 'is_main')
         widgets = {
-            'birthday': forms.SelectDateWidget(years=range(1900, 2020)),
-            'phone': forms.TextInput(attrs={'disabled': True}),
+            'birthday': forms.SelectDateWidget(years=range(1970, 2020)),
         }
--- pets/views.py
+++ pets/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Pet
 from .forms import PetCreateForm, PetChangeForm
@@ -29,18 +29,34 @@ class PetDetailView(LoginRequiredMixin, DetailView):
             return Pet.objects.none()
 
 
-class PetUpdateView(LoginRequiredMixin, UpdateView):
+class PetUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Pet
     form_class = PetChangeForm
     template_name = 'pet_edit.html'
     login_url = 'login'
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
-class PetDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        pet = self.get_object()
+        if self.request.user == pet.owner:
+            return True
+        return False
+
+
+class PetDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Pet
     template_name = 'pet_delete.html'
     success_url = reverse_lazy('pets')
     login_url = 'login'
+
+    def test_func(self):
+        pet = self.get_object()
+        if self.request.user == pet.owner:
+            return True
+        return False
 
 
 class PetCreateView(LoginRequiredMixin, CreateView):
