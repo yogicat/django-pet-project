f2b3e70fbe5185c98ce8a50a8eb7b2781be149df
yogicat <dahe.oh@gmail.com>
2019-05-06 17:49:49 +0900

    edit urlpatterns

--- petproject/urls.py
+++ petproject/urls.py
@@ -10,8 +10,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('pets/', include('pets.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] += static(settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT)
