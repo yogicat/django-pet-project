e9e4fbdd17318d807fea545a97cb0c8d3c1aa4a2
yogicat <dahe.oh@gmail.com>
2019-05-06 17:58:47 +0900

    typo

--- petproject/urls.py
+++ petproject/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('pets/', include('pets.urls')),
-] += static(settings.MEDIA_URL,
-            document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
